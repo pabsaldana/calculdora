42248ce0606624c5b375def366f5d542cf94a38d
pabsaldana <pabl.saldana@profesor.duoc.cl>
2026-06-02 09:16:55 -0400

    Create Calculadora.py

---
+++ Calculadora.py
@@ -0,0 +1,45 @@
+def sumar():
+    print("Sumar")
+
+def restar():
+    print("Restar")
+
+def dividir():
+    print("Dividir")
+
+def multiplicar():
+    print("Multiplicar")
+
+def elevar():
+    print("Elevar")
+
+def raiz_cuadrada():
+    print("Raiz Cuadrada")
+
+print("welcome to my calculator")
+
+print("1) Sumar")
+print("2) Restar")
+print("3) Dividir")
+print("4) Multiplicar")
+print("5) Elevar")
+print("6) Raiz Cuadrada")
+print("0) Salir")
+
+opcion=int(input("Ingrese opcion a trabajar "))
+if opcion==1:
+    sumar()
+elif opcion==2:
+    restar()
+elif opcion==3:
+    dividir()
+elif opcion==4:
+    multiplicar()
+elif opcion==5:
+    elevar()
+elif opcion==6:
+    raiz_cuadrada()
+elif opcion==0:
+    print("Salir")
+else:
+    print("Opcion no encontrada")
