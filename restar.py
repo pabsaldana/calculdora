4d45a18c6460e3f7b6bbff0d2839bade0a7e23fd
ivansot122 <i.soto2@duocuc.cl>
2026-06-02 09:51:17 -0400

    Create restar.py

---
+++ restar.py
@@ -0,0 +1,7 @@
+def restar():
+    print("restar")
+    a = float(input("Ingrese el primer número: "))
+    b = float(input("Ingrese el segundo número: "))
+    print(f"Resultado: {a} - {b} = {a - b}")
+    print("gracias por usar mi calculadora")
+    
